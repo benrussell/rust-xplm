97ae808925a142ee4155da17b23c4a5cbc28d462
Ben Russell <br@x-plugins.com>
2023-06-10 01:26:30 +1000

    Refac plugin message trait into small slices so we select only what we need.

--- src/messages.rs
+++ src/messages.rs
@@ -1,9 +1,11 @@
+
 //use crate::debugln;
 
 use crate::data;
 use crate::data::borrowed; //::{DataRef, FindError};
 use crate::data::{DataRead, StringRead};
 //use crate::data::{ArrayRead, DataRead, ReadOnly, ReadWrite, StringRead};
+
 
 enum XPlaneMessage{
     PlaneCrashed,
@@ -45,7 +47,9 @@ impl XPlaneMessage{
 }
 
 
-pub trait XPlaneMessageFilter: crate::plugin::Plugin {
+
+
+pub trait FilterPlaneLoaded: crate::plugin::Plugin {
 
     #[allow(unused_variables)]        
     fn rx_message(&mut self,
@@ -53,58 +57,70 @@ pub trait XPlaneMessageFilter: crate::plugin::Plugin {
         message: i32, // used to filted into sub functions
         param: *mut ::std::os::raw::c_void){
 
-        //debugln!("trait MessageCallback rx xplane message");
+        // debugln!("rust-xplm: messages.rs: trait FilterPlaneLoaded rx_message: msg:{} param:{}", message, param as i32);
 
         match XPlaneMessage::from_xplm(message){
-            Some(XPlaneMessage::PlaneCrashed) => self.msg_plane_crashed(),
             Some(XPlaneMessage::PlaneLoaded) => self.msg_plane_loaded(param as u32),
-            Some(XPlaneMessage::AirportLoaded) => self.msg_airport_loaded(),
-            Some(XPlaneMessage::SceneryLoaded) => self.msg_scenery_loaded(),
-            Some(XPlaneMessage::AirplaneCountChanged) => self.msg_airplane_count_changed(),
             Some(XPlaneMessage::PlaneUnloaded) => self.msg_plane_unloaded(param as u32),
-            Some(XPlaneMessage::WillWritePrefs) => self.msg_will_write_prefs(),
-            Some(XPlaneMessage::LiveryLoaded) => self.prepare_msg_livery_loaded(param as u32),
-            Some(XPlaneMessage::EnteredVR) => self.msg_entered_vr(),
-            Some(XPlaneMessage::ExitingVR) => self.msg_exiting_vr(),
-            Some(XPlaneMessage::ReleasePlanes) => self.msg_release_planes(),
-            Some(XPlaneMessage::FmodBankLoaded) => self.msg_fmod_bank_loaded(param as u32),
-            Some(XPlaneMessage::FmodBankUnloading) => self.msg_fmod_bank_unloading(param as u32),
-            Some(XPlaneMessage::DatarefsAdded) => self.msg_datarefs_added(param as u32),
-            None => (),
+            _ => {},
         }
-
     }
 
-
-    // https://developer.x-plane.com/sdk/XPLMPlugin/#XPLM_MSG_PLANE_CRASHED
-    
-    fn msg_plane_crashed(&mut self); // param ignored
-    
-
-    #[allow(unused_variables)]
-    fn msg_plane_loaded(&mut self, param: u32){
-        // param is index of plane being loaded
-    }
-
-    fn msg_airport_loaded(&mut self); // param ignored
-
-    // Use datarefs to determine the new scenery files that were loaded.
-    fn msg_scenery_loaded(&mut self); // param ignored
-
-    // This message is sent whenever the user adjusts the number of X-Plane aircraft models. 
-    // You must use XPLMCountPlanes to find out how many planes are now available. 
-    // This message will only be sent in XP7 and higher because in XP6 the number of aircraft is not user-adjustable.         
-    fn msg_airplane_count_changed(&mut self); // param ignored
+    fn msg_plane_loaded(&mut self, param: u32);
 
     // The parameter contains the index number of the plane being unloaded; 0 indicates the user’s plane. 
     // The parameter is of type int, passed as the value of the pointer. 
     // (That is: the parameter is an int, not a pointer to an int.)
     #[allow(unused_variables)]
-    fn msg_plane_unloaded(&mut self, param: u32 ){
+    fn msg_plane_unloaded(&mut self, param: u32 );
         // param is int value        
+
+}
+
+
+
+
+pub trait FilterVR: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterVR rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::EnteredVR) => self.msg_entered_vr(),
+            Some(XPlaneMessage::ExitingVR) => self.msg_exiting_vr(),
+            _ => {},
+        }
     }
 
-    fn msg_will_write_prefs(&mut self); // param ignored
+    fn msg_entered_vr(&mut self); // param ignored
+
+    fn msg_exiting_vr(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterLivery: crate::plugin::Plugin {
+    
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterLivery rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::LiveryLoaded) => self.prepare_msg_livery_loaded(param as u32),
+            _ => {},
+        }
+    }
 
     // This function retrieves the livery data as strings from the X-Plane datarefs.
     #[allow(unused_variables)]
@@ -116,7 +132,8 @@ pub trait XPlaneMessageFilter: crate::plugin::Plugin {
             //sim/aircraft/view/acf_livery_index int
             //sim/aircraft/view/acf_livery_path byte[1024]
 
-            let dref_livery_index: borrowed::DataRef<u32, data::ReadOnly> = borrowed::DataRef::find("sim/aircraft/view/acf_livery_index").expect("bad dref: livery index");            
+            //FIXME: Find every time?
+            let dref_livery_index: borrowed::DataRef<u32, data::ReadOnly> = borrowed::DataRef::find("sim/aircraft/view/acf_livery_index").expect("bad dref: livery index");
             let dref_livery_path: borrowed::DataRef<[u8], data::ReadOnly> = borrowed::DataRef::find("sim/aircraft/view/acf_livery_path").expect("bad dref: livery path");
 
             let livery_index: u32 = dref_livery_index.get();
@@ -128,31 +145,207 @@ pub trait XPlaneMessageFilter: crate::plugin::Plugin {
     // This is called by prepare_msg_livery_loaded(...)
     fn msg_livery_loaded(&mut self, param: u32, livery_index: u32, livery_path: &str);
 
-    fn msg_entered_vr(&mut self); // param ignored
+}
 
-    fn msg_exiting_vr(&mut self); // param ignored
+
+
+
+pub trait FilterFmod: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterFmod rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::FmodBankLoaded) => self.msg_fmod_bank_loaded(param as u32),
+            Some(XPlaneMessage::FmodBankUnloading) => self.msg_fmod_bank_unloading(param as u32),
+            _ => {},
+        }
+    }
+
+    #[allow(unused_variables)]
+    fn msg_fmod_bank_loaded(&mut self, param: u32);
+        // The parameter is the XPLMBankID enum in XPLMSound.h, 0 for the master bank and 1 for the radio bank.
+    
+    #[allow(unused_variables)]
+    fn msg_fmod_bank_unloading(&mut self, param: u32);
+        // The parameter is the XPLMBankID enum in XPLMSound.h, 0 for the master bank and 1 for the radio bank.
+    
+}
+
+
+
+
+pub trait FilterAirportLoaded: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterAirportLoaded rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::AirportLoaded) => self.msg_airport_loaded(),
+            _ => {},
+        }
+    }
+
+    fn msg_airport_loaded(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterWritePrefs: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterWritePrefs rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::WillWritePrefs) => self.msg_will_write_prefs(),
+            _ => {},
+        }
+    }
+
+    fn msg_will_write_prefs(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterReleasePlanes: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterReleasePlanes rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::ReleasePlanes) => self.msg_release_planes(),
+            _ => {},
+        }
+    }
 
     fn msg_release_planes(&mut self); // param ignored
         //multiplayer interop function..
-        
-    #[allow(unused_variables)]
-    fn msg_fmod_bank_loaded(&mut self, param: u32){
-        // The parameter is the XPLMBankID enum in XPLMSound.h, 0 for the master bank and 1 for the radio bank.
+
+}
+
+
+
+
+pub trait FilterSceneryLoaded: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterWritePrefs rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::SceneryLoaded) => self.msg_scenery_loaded(),
+            _ => {},
+        }
+    }
+
+    // Use datarefs to determine the new scenery files that were loaded.
+    fn msg_scenery_loaded(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterPlaneCrashed: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterPlaneCrashed rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::PlaneCrashed) => self.msg_plane_crashed(),
+            _ => {},
+        }
+    }
+
+// https://developer.x-plane.com/sdk/XPLMPlugin/#XPLM_MSG_PLANE_CRASHED
+    fn msg_plane_crashed(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterAirplaneCountChanged: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterAirplaneCountChanged rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::AirplaneCountChanged) => self.msg_airplane_count_changed(),
+            _ => {},
+        }
+    }
+
+    // This message is sent whenever the user adjusts the number of X-Plane aircraft models. 
+    // You must use XPLMCountPlanes to find out how many planes are now available. 
+    // This message will only be sent in XP7 and higher because in XP6 the number of aircraft is not user-adjustable.         
+    fn msg_airplane_count_changed(&mut self); // param ignored
+
+}
+
+
+
+
+pub trait FilterDatarefsAdded: crate::plugin::Plugin {
+
+    #[allow(unused_variables)]        
+    fn rx_message(&mut self,
+        from: xplm_sys::XPLMPluginID, //always from X-Plane
+        message: i32, // used to filted into sub functions
+        param: *mut ::std::os::raw::c_void){
+
+        // debugln!("rust-xplm: messages.rs: trait FilterDatarefsAdded rx_message: msg:{} param:{}", message, param as i32);
+
+        match XPlaneMessage::from_xplm(message){
+            Some(XPlaneMessage::DatarefsAdded) => self.msg_datarefs_added(param as u32),
+            _ => {},
+        }
     }
 
     #[allow(unused_variables)]
-    fn msg_fmod_bank_unloading(&mut self, param: u32){
-        // The parameter is the XPLMBankID enum in XPLMSound.h, 0 for the master bank and 1 for the radio bank.
-    }
-
-    #[allow(unused_variables)]
-    fn msg_datarefs_added(&mut self, param: u32){
+    fn msg_datarefs_added(&mut self, param: u32);
         /*
         Sent to your plugin per-frame (at-most) when/if datarefs are added. It will include the new data ref total count so that your plugin can keep a local cache of the total, see what’s changed and know which ones to inquire about if it cares.
         This message is only sent to plugins that enable the XPLM_WANTS_DATAREF_NOTIFICATIONS feature.
         */
-    }
-
 
 }
 
